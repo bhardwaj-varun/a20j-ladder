827e0b55fa89bc8fea0f17b635c95ae93d757d51
bhardwaj-varun <42834110+bhardwaj-varun@users.noreply.github.com>
2024-02-26 20:24:03 +0530

    Add codeforces  959A - Mahmoud and Ehab and the even-odd game .cpp

---
+++ 959A - Mahmoud and Ehab and the even-odd game .cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/959/A
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    long long n;
+    cin>>n;
+    if(n%2==0) cout<<"Mahmoud";
+    else cout<<"Ehab";
+    return 0;
+} 
