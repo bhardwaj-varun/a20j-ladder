35c3a38c473eb09c82a52c59f8759a8c77242252
bhardwaj-varun <42834110+bhardwaj-varun@users.noreply.github.com>
2024-02-26 20:42:49 +0530

    Create 1186A - Vus the Cossack and a Contest .cpp

---
+++ 1186A - Vus the Cossack and a Contest .cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/1186/A
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    //find the no of divisors of the given number
+
+    int n,m,k;
+    cin>>n>>m>>k;
+    if(m>=n && k>=n)
+        cout<<"Yes";
+    else
+        cout<<"No";   
+} 
